1e8addf6fe59e75a4a5b53f06f4a6d41a12aaa3b
Mark Strickland and Young Liu <mark@>
2019-01-24 08:52:08 -0800

    Add check for year divisible by 100.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 var leapYear = function(year) {
-  if (year % 4 === 0) {
-    return true;
+  if (year % 100 === 0) {
+    return false;
   }
-  else if (year % 100 != 0) {
-    return true;
-  }
-  else if (year % 400 === 0){
+  else if (year % 4 === 0) {
     return true;
   }
 }
